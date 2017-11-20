1e09eec897400456d0c9e1b07edd9e70bfe6a1f3
oncegranja <33437403+oncegranja@users.noreply.github.com>
2017-11-20 12:20:33 -0300

    Update ProyectosController.cs

--- Controllers/ProyectosController.cs
+++ Controllers/ProyectosController.cs
@@ -14,6 +14,7 @@ namespace Garantia_4.Controllers
     public class ProyectosController : Controller
     {
         //private DB_DESARROLLOEntities1 db = new DB_DESARROLLOEntities1();
+        [HandleError()]
 
         [HttpGet]
         public JsonResult ListOperacion(string TipOpeCms, string tipoPrograma)
